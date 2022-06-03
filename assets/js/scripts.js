b86eb027565f0a85b1a91347a5a0b57126f88cc8
Fernandognu <fernandogdac@hotmail.com>
2022-06-03 13:52:34 -0300

    Implementando Contador com JavaScript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,28 @@
-console.log("Hello World!");
-var heading1 = document.getElementsByTagName('h1')[0]
-heading1.style.color = 'red'
+var currrentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currrentNumberWrapper.innerHTML = currentNumber;
+    verifica();
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currrentNumberWrapper.innerHTML = currentNumber;
+    verifica();
+}
+
+function reset() {
+    currentNumber = 0
+    currrentNumberWrapper.innerHTML = currentNumber;
+    verifica()
+}
+
+function verifica() {
+    if (currentNumber < 0) {
+        currrentNumberWrapper.style.color = 'red';
+    } else {
+        currrentNumberWrapper.style.color = 'black';
+    }
+}
